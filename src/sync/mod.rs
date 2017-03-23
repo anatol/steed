834dd1878cfb024ef73a38b5626dc18f770cfda9
Anatol Pomozov <anatol.pomozov@gmail.com>
2017-03-23 14:30:00 -0700

    Add sync:once from rust 1.16.0

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -4,3 +4,8 @@
 pub use alloc::arc::{Arc, Weak};
 #[stable(feature = "steed", since = "1.0.0")]
 pub use core::sync::atomic;
+
+#[stable(feature = "rust1", since = "1.0.0")]
+pub use self::once::{Once, OnceState, ONCE_INIT};
+
+mod once;
