754af7b76dfe031b386171b4b3844d6ac581f402
Anatol Pomozov <anatol.pomozov@gmail.com>
2017-04-01 21:47:45 -0700

    Use more straight-forward helloworld example

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,8 +1,6 @@
-use std::{io, process};
-use std::io::Write;
-
 fn main() {
-    if io::stdout().write_all(b"Hello, world!\n").is_err() {
-        process::exit(1)
-    }
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
 }
